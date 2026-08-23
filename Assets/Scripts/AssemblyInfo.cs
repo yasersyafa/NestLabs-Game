83d505fe3c5edb09941ddb28800a15fdcfcb9507
adhityawe <awegamedev@gmail.com>
2026-08-24 02:06:14 +0700

    Revert "feat: base player feature & test gameplay"

--- Assets/Scripts/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-
-// Lets EditMode tests stage sensor readings that would otherwise need real colliders.
-[assembly: InternalsVisibleTo("NestLabs.Tests.EditMode")]
